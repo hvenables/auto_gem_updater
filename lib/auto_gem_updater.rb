a3e7582838f48cad801bf1e2d099f7ad3444d9c9
Harry Venables <harry@tagadab.com>
2019-03-18 15:51:30 +0000

    Initial Commit

---
+++ lib/auto_gem_updater.rb
@@ -0,0 +1,6 @@
+require "auto_gem_updater/version"
+
+module AutoGemUpdater
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/auto_gem_updater/version.rb
@@ -0,0 +1,3 @@
+module AutoGemUpdater
+  VERSION = "0.1.0"
+end
